4c76f965baa228a26153d93d01d40b755e7ab253
Anastasiia Lytvynenko <152660862+apovshr@users.noreply.github.com>
2026-02-11 16:13:18 -0700

    Update and rename Program.cs to lab13-3 (supporting file 2).cs

--- lab13-3 (supporting file 2).cs
+++ lab13-3 (supporting file 2).cs
@@ -5,17 +5,14 @@ namespace Task3;
 
 class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args) => BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
-
-    // Avalonia configuration, don't remove; also used by visual designer.
+    
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
 }
+
