01842fc49c5727215c80e8070009a6963ac151ad
Anastasiia Lytvynenko <152660862+apovshr@users.noreply.github.com>
2026-02-19 12:59:29 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using System;
+
+namespace AvaloniaFunctionApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace();
+    }
+}
